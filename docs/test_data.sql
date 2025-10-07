57798fd8f38343f1aa127212922d9adf2fc07176
xiangxn <xiangxn@163.com>
2025-10-08 01:31:58 +0800

    fix bug for test_data

--- docs/test_data.sql
+++ docs/test_data.sql
@@ -6,6 +6,6 @@ INSERT INTO users (tg_id,tg_name,tg_chat_id,tg_thread_id) VALUES
 
 INSERT INTO user_strategies (user_id,strategy_type,symbol,period,params,is_active) VALUES
 (1,"VolatilitySpike","*","5m",'{"volume": 0,"turnover": 20000000,"amplitudeMultiple": 5}',1),
-(1,"ConsecutiveMove","*","1h",'{"count": 5,"turnover": 20000000}',1),
-(1,"FundingRate","*","all",'{"fundingRate": 0.0005}',1)
+(2,"ConsecutiveMove","*","1h",'{"count": 5,"turnover": 20000000}',1),
+(3,"FundingRate","*","all",'{"fundingRate": 0.0005}',1)
 ;
